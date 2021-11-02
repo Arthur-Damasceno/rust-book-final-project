dbef55b26d278962d28db74f7e8443e459a7357c
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-02 18:22:48 -0300

    handle requests

---
+++ src/handle_connection.rs
@@ -0,0 +1,9 @@
+use std::{io::Read, net::TcpStream};
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use std::{io::Error, net::TcpListener};
 
+mod handle_connection;
+
+use handle_connection::handle_connection;
+
 pub fn run(host: &str, port: u16) -> Result<(), Error> {
     let listener = TcpListener::bind(format!("{}:{}", host, port))?;
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
 
     Ok(())
