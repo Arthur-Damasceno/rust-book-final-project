78699a86b0acf961ac9266bd9b003467f06807cd
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-02 17:57:38 -0300

    receiving streams from listener

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
-pub fn run() {
-    println!("Listening on port ...")
+use std::{io::Error, net::TcpListener};
+
+pub fn run(host: &str, port: u16) -> Result<(), Error> {
+    let listener = TcpListener::bind(format!("{}:{}", host, port))?;
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    web_server::run();
+    let host = "127.0.0.1";
+    let port = 8000;
+
+    web_server::run(host, port).unwrap();
 }
