2276aab7fa42ac6e834f1e75a9d4d72115671ff1
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-03 10:51:11 -0300

    returning html

--- src/http/handle_connection.rs
+++ src/http/handle_connection.rs
@@ -1,14 +1,19 @@
 use std::{
+    fs,
     io::{Read, Write},
     net::TcpStream,
 };
 
 pub fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("public/hello.html").unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents,
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
