c268a1e191b639d9068f1cc0bfbe49b4b512f149
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-03 12:19:57 -0300

    feat: returning 404 html for requests other than GET /

--- src/http/handle_connection.rs
+++ src/http/handle_connection.rs
@@ -9,7 +9,14 @@ pub fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let response = Response::html("public/hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let response = if buffer.starts_with(get) {
+        Response::html("public/hello.html")
+    } else {
+        Response::html("public/404.html")
+    }
+    .unwrap();
 
     stream.write(response.to_string().as_bytes()).unwrap();
     stream.flush().unwrap();
