0d6ca5021fd8e94b8983670b0868dda6e67fd76d
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-03 16:43:28 -0300

    chore: sleep thread on GET /sleep

--- src/http/handle_connection.rs
+++ src/http/handle_connection.rs
@@ -1,6 +1,8 @@
 use std::{
     io::{Read, Write},
     net::TcpStream,
+    thread,
+    time::Duration,
 };
 
 use super::{response::Response, status::Status};
@@ -10,12 +12,21 @@ pub fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let response = if buffer.starts_with(get) {
         Response::html("public/hello.html").unwrap()
+    } else if buffer.starts_with(sleep) {
+        let mut response = Response::html("public/404.html").unwrap();
+        response.status(Status::NotFound);
+
+        thread::sleep(Duration::from_secs(5));
+
+        response
     } else {
         let mut response = Response::html("public/404.html").unwrap();
         response.status(Status::NotFound);
+
         response
     };
 
