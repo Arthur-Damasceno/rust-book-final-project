52a531289840860139cdfc6219ffb8424116d067
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-03 17:09:57 -0300

    feat: thread pool new function

--- thread_pool/src/lib.rs
+++ thread_pool/src/lib.rs
@@ -1,7 +1,23 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandler<()>>,
+}
 
 impl ThreadPool {
-    pub fn new() -> Self {
-        Self
+    /// Create a new Thread Pool.
+    /// The size is the number of the threads in the pool.
+    /// 
+    /// # Panics
+    /// 
+    /// The ´new´ function will panic if the size is zero.
+    pub fn new(size: usize) -> Self {
+        if size == 0 {
+            panic!("Must have at least one thread");
+        }
+
+        let threads = Vec::with_capacity(size);
+
+        Self { threads }
     }
 }
