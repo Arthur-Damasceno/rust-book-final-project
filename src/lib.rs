130edbf64b7c015da5be673ca0da18d7586fd43b
Arthur Damasceno <damascenoarthur653@gmail.com>
2021-11-10 09:56:13 -0300

    chore: add async runtime (async-std)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::{io::Error, net::TcpListener};
-use thread_pool::ThreadPool;
 
 mod http;
 
@@ -7,14 +6,11 @@ use http::handle_connection;
 
 pub fn run(host: &str, port: u16) -> Result<(), Error> {
     let listener = TcpListener::bind(format!("{}:{}", host, port))?;
-    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
 
-        pool.execute(move || {
-            handle_connection(&mut stream);
-        })
+        handle_connection(&mut stream);
     }
 
     Ok(())
